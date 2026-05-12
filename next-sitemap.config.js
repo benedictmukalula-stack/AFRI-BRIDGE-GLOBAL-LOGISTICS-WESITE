d8f0c9fecccab9131e346a2117f498d8cc036994
Benedict Mukalula <249328655+benedictmukalula-stack@users.noreply.github.com>
2026-05-12 10:40:34 +0200

    add sitemap and robots SEO foundation

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://afri-bridge-global-logistics-websit.vercel.app',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+}
